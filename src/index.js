53e8fd32ad25ae03049a92d02c73d22a0b7c759e
Dsblima <dsbl@ecomp.poli.br>
2022-01-04 23:08:04 -0300

    Create User

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+app.use(express.json());
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const userALreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (userALreadyExists) {
+    return response.status(400).json({error: "The username already exists!!"});
+  }
+
+  const user = {
+    id:uuidv4(),
+    name,
+    username,
+    todos:[]
+  };
+
+  users.push(user);
+  
+  return response.status(201).json({createdUser: user});
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
