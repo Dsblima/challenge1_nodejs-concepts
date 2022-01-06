ccd772d44644a364f51719064d3b1a94bc4f9c0d
Dsblima <dsbl@ecomp.poli.br>
2022-01-05 22:52:14 -0300

    Get todos

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({error: "There is not a user with this username!!"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.use(express.json());
@@ -39,7 +49,9 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+
+  return response.send(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
